514f8b04cd26cba8087d421623f860d9cc0b934a
Zain Bin Tanveer <56861504+zainbt@users.noreply.github.com>
2023-09-06 23:18:04 +0500

    Router and theme added

---
+++ src/components/MainNavigation/SearchTabs/SearchSellInput.jsx
@@ -0,0 +1,34 @@
+import { React, useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { BarButtonsWrapper, SearchBarWrapper, SearchButtonWrapper, SearchTextWrapper } from "../../StyledComponents";
+
+export const SearchSellInput = (props) => {
+    const [query, setQuery] = useState("");
+    const { value, index, placeholder } = props; // value is the current tab index, index is the index of the tab   
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+        >
+            <SearchBarWrapper>
+                <SearchTextWrapper
+                    size="small"
+                    id="search-bar"
+                    placeholder={placeholder}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    inputProps={{
+                        className: "search-bar",
+                    }}
+                    variant="outlined"
+                    fullWidth
+                />
+                <BarButtonsWrapper variant="contained" size="medium">
+                    Next
+                </BarButtonsWrapper>
+            </SearchBarWrapper >
+        </div>
+    );
+}
